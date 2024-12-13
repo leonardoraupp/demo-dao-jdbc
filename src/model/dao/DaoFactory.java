c937050d188f9063f5647556d214db2a4c27df9a
Leonardo Raupp <leonardoraupprodrigues@hotmail.com>
2024-12-13 20:09:24 -0300

    improvement on DaoFactory.

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -7,13 +7,12 @@ import model.dao.impl.SellerDaoJDBC;
 import java.sql.Connection;
 
 public class DaoFactory {
-    private static Connection conn = DB.getConnection();
 
     public static SellerDao createSellerDao() {
-        return new SellerDaoJDBC(conn);
+        return new SellerDaoJDBC(DB.getConnection());
     }
 
     public static DepartmentDao createDepartmentDao() {
-        return new DepartmentDaoJDBC(conn);
+        return new DepartmentDaoJDBC(DB.getConnection());
     }
 }
